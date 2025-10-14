4d08a1849cfe7269db7f61815e52ff9c245933b2
Ashish Kumar <ashishkumar@Ashishs-MacBook-Pro.local>
2025-10-15 03:50:08 +0530

    metrics

--- logging/build/app.py
+++ logging/build/app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, abort
 import time
 import os
 from werkzeug.serving import WSGIRequestHandler
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def home():
